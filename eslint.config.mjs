c2ccb6207db161578912f40b8e07a857a3511e73
Jeancarlo Morales <72767265+jeancdevx@users.noreply.github.com>
2025-10-23 02:02:31 -0500

    Revert "Chore/setup project"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,18 @@
-import nextVitals from 'eslint-config-next/core-web-vitals'
-import nextTs from 'eslint-config-next/typescript'
-import prettier from 'eslint-config-prettier/flat'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  prettier,
-  {
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    }
-  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts'
-  ])
-])
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
-export default eslintConfig
+export default eslintConfig;
